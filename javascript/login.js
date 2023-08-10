31451c0ecc15e2f8cf0d45480db97de8789a750c
Prabhat Sharma <64638283+Prabhat7002@users.noreply.github.com>
2023-08-10 17:49:41 +0530

    Add login feature and logged in page

---
+++ javascript/login.js
@@ -0,0 +1,8 @@
+function login() {
+    window.location.href = "logggedin.html";
+}
+
+function signup(){
+    alert("You have sucessfully Signed Up!!");
+    window.location.href = "login.html";
+}
